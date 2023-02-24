d0157e53bf75bd1bfa5e2b36b9815fe24cd35a15
Igor R <88985765+rpigor@users.noreply.github.com>
2023-02-24 20:53:56 -0300

    updated pin placement

--- synthesis/physical/3_pin_clock.tcl
+++ synthesis/physical/3_pin_clock.tcl
@@ -13,13 +13,13 @@
 place_design
 
 ##Place pins in the chip sides
-edit_pin -side Top -layer 4 -spread_type center -spacing 48 -pin {{wb_dat_o[*]} uart_txd_o uart_rxd_i spi_sclk_o spi_mosi_o spi_miso_i {spi_cs_o[*]}}
+edit_pin -side Top -layer 4 -spread_type center -spacing 48 -pin {uart_txd_o uart_rxd_i spi_sclk_o spi_mosi_o spi_miso_i {spi_cs_o[*]} {wb_dat_o[*]}}
 
-edit_pin -side Left -layer 3 -spread_type center -spacing 48 -pin {{wb_adr_o[*]} wb_we_o twi_sda_io twi_scl_io {ext_ack_o[*]}}
+edit_pin -side Left -layer 3 -spread_type center -spacing 48 -pin {twi_sda_io twi_scl_io {wb_adr_o[*]} wb_we_o {ext_ack_o[*]}}
 
-edit_pin -side Bottom -layer 4 -spread_type center -spacing 48 -pin {{wb_dat_i[*]} wb_stb_o wb_cyc_o wb_ack_i {ext_irq_i[*]}}
+edit_pin -side Bottom -layer 4 -spread_type center -spacing 48 -pin {wb_stb_o wb_cyc_o wb_ack_i {wb_dat_i[*]} {ext_irq_i[*]}}
 
-edit_pin -side Right -layer 3 -spread_type center -spacing 48 -pin {clk_i rst_i {pwm_o[*]} {freq_gen_o[*]} {wb_sel_o[*]} {gpio_o[*]} {gpio_i[*]}}
+edit_pin -side Right -layer 3 -spread_type center -spacing 48 -pin {clk_i rst_i {wb_sel_o[*]} {gpio_o[*]} {gpio_i[*]} {pwm_o[*]} {freq_gen_o[*]}}
 
 opt_design -pre_cts
 
